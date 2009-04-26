778428d176cb356d3ebe9e5a1817b0fe67381128
Katsuyoshi Ito <kito@itosoft.com>
2009-04-26 16:26:02 +0900

    implement opencode

--- tools/opencode/opencode.m
+++ tools/opencode/opencode.m
@@ -1,10 +1,22 @@
 #import <Foundation/Foundation.h>
+#import "SourceCodeOpener.h"
+#import "Server.h"
 
 int main (int argc, const char * argv[]) {
     NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    NSRunLoop * runLoop = [NSRunLoop currentRunLoop];
 
-    // insert code here...
-    NSLog(@"Hello, World!");
+    Server *server = [Server new];
+    NSError * error = nil;
+    [server setType:@"_iunittest_opencode._tcp."];
+    if (![server start:&error] ) {
+        NSLog(@"Error starting server: %@", error);
+    } else {
+        NSLog(@"Starting server on port %d", [server port]);
+    }
+
+    [runLoop run];
+    [server release];
     [pool drain];
     return 0;
 }
