770f6908e48d5ffa5063cd1f952ce13f6601390d
Katsuyoshi Ito <kito@itosoft.com>
2009-05-14 08:37:53 +0900

    add option to template files

--- templates/File Templates/iUnitTest/UITabBarAndNavigationBarBasedTest subclass.pbfiletemplate/class.m
+++ templates/File Templates/iUnitTest/UITabBarAndNavigationBarBasedTest subclass.pbfiletemplate/class.m
@@ -52,4 +52,15 @@
     ASSERT_FAIL(@"not implemented");
 }
 
+
+#pragma mark -
+#pragma mark Option
+
+// Uncomment it, if you want to test this class except other passed test classes.
+//#define TESTS_ALWAYS
+#ifdef TESTS_ALWAYS
+- (void)testThisClassAlways { ASSERT_FAIL(@"fail always"); }
++ (BOOL)forceTestsAnyway { return YES; }
+#endif
+
 @end
