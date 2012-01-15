70b518764ef1f78f63d612599d373716afbbfc56
Katsuyoshi Ito <kito@itosoft.com>
2012-01-15 21:56:45 +0900

    added a script for feedback changed codes.

---
+++ iUnitTest/script/sync_source.rb
@@ -0,0 +1,27 @@
+# -*- coding:UTF-8 -*-
+require "fileutils"
+
+path = File.expand_path(File.dirname __FILE__)
+
+dst_path = File.join(path, "../tmp")
+
+FileUtils.mkdir_p dst_path
+
+Dir.chdir(dst_path) do
+  unless File.exist? "iunittest"
+    system "git clone git@github.com:katsuyoshi/iunittest.git"
+    Dir.chdir "iunittest" do
+      system "git checkout -b xcode4 origin/xcode4"
+    end
+  end
+  src_path = "../iUnitTest/iunittest"
+  dst_path = "iunittest/iUnitTest/iUnitTest"
+  FileUtils.cp_r src_path, dst_path
+  Dir.chdir "iunittest" do
+    [
+      "iUnitTest/iUnitTest/iunittest/IUTMainWindow.xib"
+    ].each do |f|
+      system "git checkout #{f}"
+    end
+  end
+end
